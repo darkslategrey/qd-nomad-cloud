7571115f8f8605f48f8a8c07cf02f0fb0060f07f
Greg <gregory@courseur.com>
2018-06-29 12:38:33 +0200

    disable(50-lb-traeffik)

--- 50-lb-traeffik/instance_group.tf
+++ 50-lb-traeffik/instance_group.tf
@@ -1,9 +1,9 @@
 resource "google_compute_health_check" "autohealing" {
   name                = "traefik-check"
-  check_interval_sec  = 10
-  timeout_sec         = 10
-  healthy_threshold   = 3
-  unhealthy_threshold = 3
+  # check_interval_sec  = 10
+  # timeout_sec         = 10
+  # healthy_threshold   = 3
+  # unhealthy_threshold = 3
 
   tcp_health_check {
     port         = "80"
@@ -22,7 +22,7 @@ resource "google_compute_region_instance_group_manager" "traefik-cluster" {
 
   named_port {
     name = "http"
-    port = 443
+    port = 80
   }
 
   auto_healing_policies {
@@ -31,19 +31,19 @@ resource "google_compute_region_instance_group_manager" "traefik-cluster" {
   }
 }
 
-resource "google_compute_region_autoscaler" "traefik" {
-  count = "${length(var.deploy_region)}"
-  name   = "traefik-scaler"
-  region = "${element(var.deploy_region, count.index)}"
-  target = "${element(google_compute_region_instance_group_manager.traefik-cluster.*.self_link, count.index)}"
+# resource "google_compute_region_autoscaler" "traefik" {
+#   count = "${length(var.deploy_region)}"
+#   name   = "traefik-scaler"
+#   region = "${element(var.deploy_region, count.index)}"
+#   target = "${element(google_compute_region_instance_group_manager.traefik-cluster.*.self_link, count.index)}"
 
-  autoscaling_policy = {
-    max_replicas    = 3
-    min_replicas    = 1
-    cooldown_period = 60
+#   autoscaling_policy = {
+#     max_replicas    = 3
+#     min_replicas    = 1
+#     cooldown_period = 60
 
-    load_balancing_utilization {
-      target = 0.8
-    }
-  }
-}
+#     load_balancing_utilization {
+#       target = 0.8
+#     }
+#   }
+# }
